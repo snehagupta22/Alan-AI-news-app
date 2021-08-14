12307a933109327cfc339097d0547322c5cd2fd6
Sneha Gupta <snehagupta.che18@itbhu.ac.in>
2021-08-14 13:00:34 +0530

    deploy my react app to github pages

---
+++ alan_ai_news_application/src/App.js
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './Components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+import wordsToNumbers from 'words-to-numbers';
+
+const alanKey='7b127ee5228fa1783fe959360e7919052e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App=()=>{
+
+    const [newsArticles,setNewsArticles]=useState([]);
+    const [activeArticle,setActiveArticle]=useState(0);
+    const classes=useStyles();
+    useEffect(()=>{
+        alanBtn({
+            key:alanKey,
+            onCommand:({command,articles,number})=>{
+                if(command==='newHeadlines'){
+                    setNewsArticles(articles);
+                    
+                }
+                else if(command==='highlight'){
+                    setActiveArticle((prevActiveArticle)=>prevActiveArticle+1);
+                }
+                else if(command==='open'){
+                    const parsedNumber=number.length>2? wordsToNumbers(number,{fuzzy:true}):number;
+                    const article=articles[parsedNumber-1];
+                    if(parsedNumber>20){
+                        alanBtn().playText('Please try that again.')
+
+                    }
+                    else if(article){
+                        window.open(article.url,'_blank');
+                        alanBtn().playText('Opening');
+                    }
+                    
+                }
+            }
+        })
+    },[])
+
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/brand_assets/logo-horizontal/color/alan-logo-horizontal-color.png" className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    );
+}
+export default App;
---
+++ alan_ai_news_application/src/Components/NewsCard/NewsCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Card, CardActionArea,CardAction,CardContent,CardMedia,Button,Typography, CardActions} from '@material-ui/core';
+import { typography } from '@material-ui/system';
+import useStyles from './styles.js';
+import classNames from 'classnames';
+
+
+const NewsCard = ({article:{description, publishedAt, source,title,url,urlToImage},i,activeArticle}) => {
+    const classes=useStyles();
+    
+    return (
+        <Card className={classNames(classes.card, activeArticle===i? classes.activeCard:null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://image.shutterstock.com/image-vector/breaking-news-background-planet-600w-667420906.jpg'}/>
+                    <div className={classes.details}>
+                        <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                        <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                    </div>   
+                        <Typography   className={classes.title} gutterBottom variant="h5">{title}</Typography>
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                        </CardContent>
+            </CardActionArea>
+            <CardActions  className={classes.cardActions }>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{i+1}</Typography>
+            </CardActions>
+
+        </Card>
+    )
+}
+
+export default NewsCard;
